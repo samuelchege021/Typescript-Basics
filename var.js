afd6aa0662173732af7ee3611bdcee0f43ce7ec5
samuelchege021 <samuelngangachege8243@gmail.comm>
2026-02-06 11:08:13 +0300

    c

--- var.js
+++ var.js
@@ -23,3 +23,73 @@ if (count > 0) {
     var message = "Count is positive";
     console.log(message);
 }
+// 3. const
+// Similar to let in terms of scoping,
+//  const is used for variables that should not be reassigned after their initial value.
+//  Attempting to reassign 
+// a const variable results in a compile-time error.
+var country = "kenya";
+console.log(country);
+var jina = "ann karimi";
+var myage = 22;
+var active = true;
+function displayinfo(name, age) {
+    return "my name is ".concat(name, " and my age is ").concat(age);
+}
+var greeting = displayinfo(jina, myage);
+console.log(greeting);
+// local scodpe
+function localScopeExample() {
+    var localVar = "I am local";
+    console.log(localVar); // Accessible here
+}
+localScopeExample();
+var globalVar = "I am global";
+function accessibleglobal() {
+    console.log("i am global var " + globalVar);
+}
+accessibleglobal();
+// 3. Class Scope
+// Variables declared within a class are accessible to all members (methods) of that class.
+// class employee{
+// salary:number=6000;
+// }
+function sayHi() {
+    console.log("Hi");
+}
+var result = sayHi();
+sayHi();
+console.log(result); // undefined because the function does not return anything
+var username = "adjan";
+var age = 3;
+var isactive = true;
+function greetuser(name, age) {
+    return (" hello ".concat(name, " my age is ").concat(age));
+}
+var greetingd = greetuser(username, age);
+console.log(greetingd);
+var employee = /** @class */ (function () {
+    function employee() {
+        this.salary = 5000;
+    }
+    employee.prototype.printsalary = function () {
+        console.log("salary is ".concat(this.salary));
+    };
+    return employee;
+}());
+var empp = new employee();
+empp.printsalary();
+var Globalvar = 10;
+var Geeks = /** @class */ (function () {
+    function Geeks() {
+        this.classvar = 12;
+    }
+    Geeks.prototype.assignnum = function () {
+        var localvar = 13;
+        console.log('local variable' + localvar);
+    };
+    return Geeks;
+}());
+console.log('global var' + globalVar);
+var obj = new Geeks();
+obj.assignnum();
--- var.ts
+++ var.ts
@@ -2,7 +2,7 @@
 
 
 
-let sname: string = "Alice";
+let sname:string = "Alice";
 console.log(sname);
 
 
@@ -55,3 +55,181 @@ if (count>0) {
 
 
 
+// 3. const
+// Similar to let in terms of scoping,
+//  const is used for variables that should not be reassigned after their initial value.
+//  Attempting to reassign 
+// a const variable results in a compile-time error.
+const country= "kenya";   
+
+console.log(country);
+
+
+let jina :string="ann karimi";
+let myage:number=22;
+
+let active:boolean=true;
+
+function displayinfo(name:string,age:number):string {
+
+
+    return `my name is ${name} and my age is ${age}`;
+}
+
+
+
+
+let greeting =displayinfo(jina,myage);
+
+console.log(greeting);
+
+
+
+
+// local scodpe
+
+
+
+
+function localScopeExample() {
+    let localVar = "I am local";
+    console.log(localVar); // Accessible here
+  }
+
+  localScopeExample();
+
+
+
+  let globalVar = "I am global";
+
+
+
+function accessibleglobal(){
+
+    console.log("i am global var "+ globalVar);
+}
+
+accessibleglobal();
+
+
+
+
+// 3. Class Scope
+// Variables declared within a class are accessible to all members (methods) of that class.
+
+
+
+// class employee{
+
+// salary:number=6000;
+
+      
+// }
+
+
+function sayHi(): void {
+    console.log("Hi");
+}
+let result=sayHi();
+sayHi()
+console.log(result); // undefined because the function does not return anything
+
+
+
+
+
+let username:string="adjan";
+let age:number=3;
+let isactive:Boolean=true;
+
+
+
+
+function greetuser(name:string,age:number):string{
+
+
+return (` hello ${name} my age is ${age}`);
+
+
+
+
+
+}
+  let greetingd= greetuser(username,age)
+  console.log(greetingd)
+
+
+
+
+
+
+
+
+
+  class employee{
+
+
+    salary:number=5000
+
+
+    printsalary():void{
+
+
+        console.log(`salary is ${this.salary}`)
+
+
+    }
+
+
+
+
+
+
+
+  }
+
+
+
+
+const empp=new employee()
+empp.printsalary()
+
+
+
+
+
+
+let Globalvar:number=10
+
+
+class Geeks{
+
+
+private classvar:number=12
+
+
+assignnum():void{
+
+
+let localvar:number=13;
+
+console.log('local variable'+localvar)
+
+
+
+}
+
+
+
+
+
+}
+
+
+
+
+console.log('global var'+globalVar)
+
+
+const obj=new Geeks();
+obj.assignnum();
